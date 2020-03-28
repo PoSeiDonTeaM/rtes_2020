93b56faa7b0db24dde2bb3ca7bcc441de45ae6e0
Giannis Kotsakiachidis <johnkotsas@me.com>
2020-03-28 12:50:23 +0200

    Producers and Consumers work perfectly. Gettimeofday shall be integrated.

--- assignment_1/gkotsas_prod_cons.c
+++ assignment_1/gkotsas_prod_cons.c
@@ -23,9 +23,9 @@
 #include <queue>
 #include <sys/time.h>
 
-#define pro_threads 600
-#define con_threads 33
-#define MAX_QUEUE_SIZE 10
+#define pro_threads 100
+#define con_threads 50
+#define MAX_QUEUE_SIZE 100
 #define MAX_LOOPS 100
 
 using namespace std; 
@@ -40,6 +40,9 @@ queue<int> Q;
 int sum = 0;
 int consumerCount = 0;
 
+struct timeval t1, t2;
+int elapsedTime[MAX_LOOPS*pro_threads];
+
 // Getting the mutex 
 pthread_mutex_t mutex           = PTHREAD_MUTEX_INITIALIZER; 
 
@@ -48,6 +51,8 @@ pthread_cond_t  dataNotConsumed = PTHREAD_COND_INITIALIZER;
 
 int main ()
 {
+    
+  
   
   pthread_t pro[pro_threads], con[con_threads];
 
@@ -72,6 +77,11 @@ int main ()
   }
   
   cout << "Wow, you made it to the end sailor! Congrats." << endl;
+  
+  cout << "Let me now print you the elapsed times" << endl;
+  
+  for (int x=0; x<MAX_LOOPS*pro_threads; x++)
+      cout << "Time interval #" << x << ": " << elapsedTime[x] << endl;
 
   return 0;
 }
@@ -80,12 +90,13 @@ void *producer (void *q)
 {
   static int producerCount = 0;  
 
+  
 
   while(1) {
       
     pthread_mutex_lock (&mutex); // Locking queue with mutex
     
-    if (Q.size() < MAX_QUEUE_SIZE && producerCount < MAX_LOOPS)
+    if (Q.size() < MAX_QUEUE_SIZE && producerCount < MAX_LOOPS*pro_threads)
     {
         // Generating a random number from 0~9 to be used in function selection
         int num = rand()%9;
@@ -94,13 +105,16 @@ void *producer (void *q)
         // Pushing the number into queue
         Q.push(num);
         
+        // Starting the timer
+        gettimeofday(&t1, NULL);
+        
         producerCount++;
         
         pthread_cond_broadcast(&dataNotProduced);
     }
     
     // If queue is full release mutex and break the loop
-    else if (producerCount == MAX_LOOPS)
+    else if (producerCount == MAX_LOOPS*pro_threads)
     {
         pthread_mutex_unlock(&mutex);
         return NULL;
@@ -109,28 +123,36 @@ void *producer (void *q)
     // If another thread is executing, then wait.
     else{
         cout << "Producer: Hold on there cowboy, another thread is executing..." << endl;
+        cout << "Queue size is: " << Q.size() << endl;
+        
         pthread_cond_wait(&dataNotConsumed, &mutex);
     }
     
     // Unlock the mutex
     pthread_mutex_unlock(&mutex);
     
-    usleep(10000);
 
   }
 }
 
 void *consumer (void *q)
 {
+    
 
   while(1) {
       
     pthread_mutex_lock (&mutex);
     
-    if(Q.size() > 0)
+    if(Q.size() > 0 && consumerCount < MAX_LOOPS*pro_threads)
     {
         // Get the data from the front of the queue
         int data = Q.front();
+        
+        // Stopping the timer
+        gettimeofday(&t2, NULL);
+        
+        // Saving the elapsedTime in an array
+        elapsedTime[MAX_LOOPS*pro_threads-1] =  (t2.tv_usec - t1.tv_usec); // sec to ms
         
         cout << "Hey, I'm Consumer-" << pthread_self() << ". I'm using the function: " << data << endl;
         
@@ -138,7 +160,7 @@ void *consumer (void *q)
         // At this stage, we just sum the data and integrate the extra functions later.
         sum += data;
         
-        // Pop the consumed data from the queue
+        // Pop the consumed data from the queue 
         Q.pop();
         
         consumerCount++;
@@ -147,7 +169,7 @@ void *consumer (void *q)
     }
     
     // Check if consumers have consumed all the threads that have been produced
-    else if(consumerCount == MAX_LOOPS)
+    else if(consumerCount == MAX_LOOPS*pro_threads)
     {
         pthread_mutex_unlock(&mutex);
         return NULL;
